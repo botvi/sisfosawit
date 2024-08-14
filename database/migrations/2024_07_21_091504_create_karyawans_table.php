7c299ea7464ffa86b559c66c2c7bde156315b57f
Yovi Ardiansyah <75218854+botvi@users.noreply.github.com>
2024-08-14 13:06:23 +0700

    Update 2024_07_21_091504_create_karyawans_table.php

--- database/migrations/2024_07_21_091504_create_karyawans_table.php
+++ database/migrations/2024_07_21_091504_create_karyawans_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('karyawans', function (Blueprint $table) {
             $table->id(); // Primary key
             $table->string('kd_identitas'); // Nama karyawan
+            $table->integer('nik'); // Nama karyawan
             $table->string('nama'); // Nama karyawan
             $table->integer('umur'); // Nama karyawan
             $table->string('jabatan'); // Jabatan karyawan
